692588b9ca11e4c65989ca73d4fed24ca3fb4efd
testforPersianMega <testformaman654@gmail.com>
2026-02-04 01:32:27 +0330

    Fix user books auth module import

--- apps/api/src/user-books/user-books.module.ts
+++ apps/api/src/user-books/user-books.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { UserBooksController } from "./user-books.controller";
 import { UserBooksService } from "./user-books.service";
 import { PrismaService } from "../common/prisma.service";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
+  imports: [AuthModule],
   controllers: [UserBooksController],
   providers: [UserBooksService, PrismaService]
 })
